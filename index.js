ea99cc156b5619e7c093982cf09d86f241d097d5
JT <jstorbit@gmail.com>
2025-11-20 18:11:35 -0700

    Finish DogImage element and made get dog image

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const getDogImage = async () => {
+  const API = `https://dog.ceo/api/breeds/image/random`;
+  const response = await fetch(API);
+  const data = await response.json();
+  return data.message;
+}
+
 const DogImage = () => {
   const $dogImage = document.createElement(`img`);
   $dogImage.alt = `Random dog image`;
   $dogImage.src = (getDogImage());
+  return $dogImage;
 }
 
 const rendor = () => {
@@ -9,6 +17,8 @@ const rendor = () => {
   $app.innerHTML = `
     <h1>Dog Images</h1>
     <DogImage></DogImage>`;
+
+  document.querySelector(`DogImage`).replaceWith(DogImage());
 }
 
 rendor();
