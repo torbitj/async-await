c534cda37509cb0f6b02b780d6b6fafb32295eee
JT <jstorbit@gmail.com>
2025-11-20 17:58:27 -0700

    Add DogImage element for rendor

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const rendor = () => {
-  
+const DogImage = () => {
+  const $dogImage = document.createElement(`img`);
+  $dogImage.alt = `Random dog image`;
+  $dogImage.src = (getDogImage());
 }
+
+const rendor = () => {
+  const $app = document.querySelector(`#app`);
+  $app.innerHTML = `
+    <h1>Dog Images</h1>
+    <DogImage></DogImage>`;
+}
+
+rendor();
